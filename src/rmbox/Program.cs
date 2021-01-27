f99262e8a22ec9e3620153e9da85b98a584eabfa
Il Harper <afanyiyu@hotmail.com>
2021-01-27 09:45:05 +0800

    feat: Add Solution

---
+++ src/rmbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ruminoid.Toolbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
