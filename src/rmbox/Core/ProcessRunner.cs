8036e9ae4979a2c36b0c6fdc110569da1c903f08
Il Harper <afanyiyu@hotmail.com>
2021-01-30 01:40:17 +0800

    chore: Add Comments

--- src/rmbox/Core/ProcessRunner.cs
+++ src/rmbox/Core/ProcessRunner.cs
@@ -1,9 +1,20 @@
-﻿using System.Composition;
+﻿using System;
+using System.Composition;
 
 namespace Ruminoid.Toolbox.Core
 {
     [Export]
     public class ProcessRunner
     {
+        /// <summary>
+        /// 执行进程。
+        /// </summary>
+        /// <param name="target">进程目标。</param>
+        /// <param name="args">进程参数。</param>
+        /// <returns>进程是否成功执行。</returns>
+        public bool Run(string target, string args)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- src/rmbox/Core/ProjectParser.cs
+++ src/rmbox/Core/ProjectParser.cs
@@ -1,9 +1,19 @@
-﻿using System.Composition;
+﻿using System;
+using System.Composition;
+using Newtonsoft.Json.Linq;
 
 namespace Ruminoid.Toolbox.Core
 {
     [Export]
     public class ProjectParser
     {
+        /// <summary>
+        /// 解析 JSON 项目文件，并执行操作。
+        /// </summary>
+        /// <param name="project">JSON 项目文件。</param>
+        public void Parse(JObject project)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
