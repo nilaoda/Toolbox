c5334f0fb0cf7e4fb12649728eff6c65f31868da
Il Harper <afanyiyu@hotmail.com>
2021-01-30 01:31:01 +0800

    feat: Add IFormatter

---
+++ src/rmbox/Formatting/IFormatter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ruminoid.Toolbox.Formatting
+{
+    public interface IFormatter
+    {
+        public FormattedEvent Format(string data);
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    sealed class FormatterAttribute : Attribute
+    {
+        public FormatterAttribute(string targets) => Targets = targets;
+
+        public readonly string Targets;
+    }
+}
