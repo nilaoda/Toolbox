bb71b7a9e5a83a207dcf53ab347182bc0ef8af2f
Il Harper <afanyiyu@hotmail.com>
2021-01-30 01:04:44 +0800

    feat: Add FormattingHelper Class

---
+++ src/rmbox/Formatting/FormattingHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Composition;
+
+namespace Ruminoid.Toolbox.Formatting
+{
+    [Export]
+    public class FormattingHelper
+    {
+        public FormattingHelper()
+        {
+        }
+
+        #region Target
+
+        private string _currentTarget = "";
+
+        public void UpdateTarget(string target) => _currentTarget = target;
+
+        #endregion
+    }
+}
