7659141f0f35b5b4f7eefe4a18799f412d0e12c4
Il Harper <afanyiyu@hotmail.com>
2021-01-30 22:07:16 +0800

    feat: ProjectParser Update

--- src/rmbox/Core/ProjectParser.cs
+++ src/rmbox/Core/ProjectParser.cs
@@ -1,11 +1,33 @@
-﻿using System;
-using System.Composition;
+﻿using System.Composition;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Linq;
+using Ruminoid.Toolbox.Helpers.CommandLine;
 
 namespace Ruminoid.Toolbox.Core
 {
     [Export]
     public class ProjectParser
     {
+        public ProjectParser(
+            CommandLineHelper commandLineHelper,
+            ILogger<ProjectParser> logger)
+        {
+            _commandLineHelper = commandLineHelper;
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// 解析 JSON 项目文件，并执行操作。
+        /// </summary>
+        public void Parse()
+        {
+            _logger.LogDebug("Parsing using projectPath from ProcessOptions.");
+
+            // ReSharper disable once PossibleNullReferenceException
+            Parse((_commandLineHelper.Options as ProcessOptions).ProjectPath);
+        }
+
         /// <summary>
         /// 解析 JSON 项目文件，并执行操作。
         /// </summary>
@@ -14,5 +36,8 @@ namespace Ruminoid.Toolbox.Core
         {
             throw new NotImplementedException();
         }
+
+        private readonly CommandLineHelper _commandLineHelper;
+        private readonly ILogger<ProjectParser> _logger;
     }
 }
