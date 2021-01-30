ad2ee86a6a5d6af607ba4aed7f6c08270d9e5c24
Il Harper <afanyiyu@hotmail.com>
2021-01-30 22:41:15 +0800

    feat: Add ProjectParseException

---
+++ src/rmbox-plugbase/Core/ProjectParseException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ruminoid.Toolbox.Core
+{
+    [Serializable]
+    public class ProjectParseException : Exception
+    {
+        public ProjectParseException()
+        {
+        }
+
+        public ProjectParseException(string message) : base(message)
+        {
+        }
+
+        public ProjectParseException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
