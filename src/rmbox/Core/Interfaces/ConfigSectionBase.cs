11c1cd06f1115c3866bf1f5d12ef47b8c5da3705
Il Harper <afanyiyu@hotmail.com>
2021-01-30 11:37:52 +0800

    refactor: ConfigSectionBase Update

---
+++ src/rmbox/Core/Interfaces/ConfigSectionBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Avalonia.Controls.Primitives;
+
+namespace Ruminoid.Toolbox.Core
+{
+    public abstract class ConfigSectionBase : TemplatedControl
+    {
+        public string Header { get; set; }
+
+        public object Config { get; set; }
+
+        public string CurrentHelpText { get; set; }
+    }
+
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public sealed class ConfigSectionAttribute : Attribute
+    {
+        public ConfigSectionAttribute(string id) => Id = id;
+
+        public readonly string Id;
+    }
+}
