0e6af1488afa3e1f1d8feaceee90a5e2b6eb7bd1
Il Harper <afanyiyu@hotmail.com>
2021-01-31 00:40:45 +0800

    feat: ProcessOptions Update

--- src/rmbox/Helpers/CommandLine/ProcessOptions.cs
+++ src/rmbox/Helpers/CommandLine/ProcessOptions.cs
@@ -5,5 +5,13 @@ namespace Ruminoid.Toolbox.Helpers.CommandLine
     [Verb("process", true, HelpText = "执行任务或任务队列。")]
     public class ProcessOptions
     {
+        [Value(0, HelpText = "JSON 项目文件的路径。", Required = true)]
+        public string ProjectPath { get; set; }
+
+        [Option("skip-version-check", Default = false, HelpText = "跳过项目文件版本检查。", Required = false)]
+        public bool SkipVersionCheck { get; set; }
+
+        [Option('o', "log-process-out", Default = false, HelpText = "显示进程输出。", Required = false)]
+        public bool LogProcessOut { get; set; }
     }
 }
