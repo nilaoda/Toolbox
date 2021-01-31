865ae23285a770d2b2fd6be186cf55051a4ccc5c
Il Harper <afanyiyu@hotmail.com>
2021-01-31 17:14:43 +0800

    feat: Add ProcessRunnerException

---
+++ src/rmbox-plugbase/Core/ProcessRunnerException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ruminoid.Toolbox.Core
+{
+    [Serializable]
+    public class ProcessRunnerException : Exception
+    {
+        public ProcessRunnerException()
+        {
+        }
+
+        public ProcessRunnerException(string message) : base(message)
+        {
+        }
+
+        public ProcessRunnerException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
