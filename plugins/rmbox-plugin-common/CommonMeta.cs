5f196a2c2a5d02ef3ddda99d974bd75563e9e026
Il Harper <afanyiyu@hotmail.com>
2021-01-31 21:10:41 +0800

    feat: Add CommonMeta

---
+++ plugins/rmbox-plugin-common/CommonMeta.cs
@@ -0,0 +1,11 @@
+﻿using Ruminoid.Toolbox.Core;
+
+namespace Ruminoid.Toolbox.Plugins.Common
+{
+    [RmboxPlugin]
+    public class CommonMeta : IMeta
+    {
+        public string Name => "Ruminoid Toolbox 基础";
+        public string Description => "提供了工具箱的基础功能。";
+    }
+}
