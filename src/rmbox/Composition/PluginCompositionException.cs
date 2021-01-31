8dc14c3537c67910671958bc62b94cfaef38abb7
Il Harper <afanyiyu@hotmail.com>
2021-01-31 13:12:37 +0800

    feat: Add PluginCompositionException

---
+++ src/rmbox/Composition/PluginCompositionException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ruminoid.Toolbox.Composition
+{
+    [Serializable]
+    public class PluginCompositionException : Exception
+    {
+        public PluginCompositionException()
+        {
+        }
+
+        public PluginCompositionException(string message) : base(message)
+        {
+        }
+
+        public PluginCompositionException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
