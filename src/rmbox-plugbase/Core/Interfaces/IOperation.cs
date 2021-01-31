fea46d781e36399ea4cc18faa9be2cc00a9d9141
Il Harper <afanyiyu@hotmail.com>
2021-01-31 16:47:21 +0800

    feat: IOperation Update

--- src/rmbox-plugbase/Core/Interfaces/IOperation.cs
+++ src/rmbox-plugbase/Core/Interfaces/IOperation.cs
@@ -6,7 +6,7 @@ namespace Ruminoid.Toolbox.Core
 {
     public interface IOperation
     {
-        public List<Tuple<string, string>> Generate(JObject config);
+        public List<Tuple<string, string>> Generate(Dictionary<string, JToken>);
 
         public List<string> RequiredConfigSections { get; }
     }
