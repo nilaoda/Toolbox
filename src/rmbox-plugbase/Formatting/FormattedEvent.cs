3156b397b7b22dda9ad93375e9591f47f568dd88
Il Harper <afanyiyu@hotmail.com>
2021-01-31 19:40:37 +0800

    feat: FormattedEvent Update

--- src/rmbox-plugbase/Formatting/FormattedEvent.cs
+++ src/rmbox-plugbase/Formatting/FormattedEvent.cs
@@ -2,9 +2,17 @@
 {
     public record FormattedEvent
     {
+        public FormattedEvent(string target, double progress, string usedTime, string remainingTime)
+        {
+            Target = target;
+            Progress = progress;
+            UsedTime = usedTime;
+            RemainingTime = remainingTime;
+        }
+
         public string Target { get; }
+        public double Progress { get; }
         public string UsedTime { get; }
         public string RemainingTime { get; }
-        public double Progress { get; }
     }
 }
