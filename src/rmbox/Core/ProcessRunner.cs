4c6a21c03d1410c01048dc0bb8a5677ad8b703e1
Il Harper <afanyiyu@hotmail.com>
2021-01-31 19:28:22 +0800

    feat: ProcessRunner Complete

--- src/rmbox/Core/ProcessRunner.cs
+++ src/rmbox/Core/ProcessRunner.cs
@@ -1,24 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Composition;
+using System.Diagnostics;
+using System.IO;
+using System.Reactive.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 using Microsoft.Extensions.Logging;
 using Ruminoid.Toolbox.Composition;
+using Ruminoid.Toolbox.Formatting;
 using Ruminoid.Toolbox.Helpers.CommandLine;
+using Ruminoid.Toolbox.Utils;
 
 namespace Ruminoid.Toolbox.Core
 {
     [Export]
-    public class ProcessRunner
+    public class ProcessRunner : IDisposable
     {
         public ProcessRunner(
             CommandLineHelper commandLineHelper,
             PluginHelper pluginHelper,
+            FormattingHelper formattingHelper,
             Logger<ProcessRunner> logger)
         {
             _commandLineHelper = commandLineHelper;
             _pluginHelper = pluginHelper;
+            _formattingHelper = formattingHelper;
             _logger = logger;
+
+            _unsubscribe = _formattingHelper.FormatData.Subscribe(
+                OnNext,
+                OnError);
         }
+
+        #region Runner
 
         /// <summary>
         /// 执行进程。
@@ -27,7 +42,73 @@ namespace Ruminoid.Toolbox.Core
         /// <param name="args">进程参数。</param>
         public void Run(string target, string args)
         {
-            throw new NotImplementedException();
+            _logger.LogDebug($"Resolving target: {target}");
+
+            string workingDirectory = StorageHelper.GetSectionFolderPath("tools");
+
+            string targetPath = Path.Combine(
+                workingDirectory,
+                target + (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? ".exe" : ""));
+
+            if (!File.Exists(targetPath))
+            {
+                string err = $"未能找到进程文件：{targetPath}。可能需要安装相关的插件以解决此问题。";
+                _logger.LogCritical(err);
+                throw new ProcessRunnerException(err);
+            }
+
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    CreateNoWindow = true,
+                    StandardErrorEncoding = Encoding.UTF8,
+                    StandardInputEncoding = Encoding.UTF8,
+                    StandardOutputEncoding = Encoding.UTF8,
+                    WorkingDirectory = workingDirectory,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    Arguments = ' ' + args,
+                    FileName = targetPath
+                },
+                EnableRaisingEvents = true
+            };
+
+            // ReSharper disable once InvokeAsExtensionMethod
+            IDisposable observable = Observable.Concat(
+                    Observable.FromEventPattern<DataReceivedEventArgs>(process, nameof(process.ErrorDataReceived)),
+                    Observable.FromEventPattern<DataReceivedEventArgs>(process, nameof(process.OutputDataReceived)))
+                .Subscribe(
+                    next =>
+                    {
+                        var (_, e) = next;
+                        if (!string.IsNullOrEmpty(e.Data)) return;
+
+                        if (((ProcessOptions) _commandLineHelper.Options).LogProcessOut)
+                            _logger.LogInformation($"[{target}]{e.Data}");
+                        _formattingHelper.ReceiveData.OnNext(new Tuple<string, string>(target, e.Data));
+                    },
+                    error => _logger.LogError(error, "进程发生了错误。"));
+
+            _logger.LogInformation($"开始运行：{targetPath} {args}");
+            
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+
+            observable.Dispose();
+
+            if (process.ExitCode != 0)
+            {
+                string err = $"{target} 程序出现错误，退出码为 {process.ExitCode}。";
+                _logger.LogCritical(err);
+                throw new ProcessRunnerException(err);
+            }
+
+            _logger.LogInformation($"{target} 运行结束，程序正常退出。");
         }
 
         /// <summary>
@@ -43,11 +124,33 @@ namespace Ruminoid.Toolbox.Core
         /// <param name="commands">指令列表。</param>
         public void Run(List<Tuple<string, string>> commands)
         {
+            _logger.LogInformation($"开始运行 {commands.Count} 条指令。");
 
+            commands.ForEach(Run);
         }
+
+        #endregion
+
+        #region Subscribe
+
+        private readonly IDisposable _unsubscribe;
+
+        private void OnError(Exception error) => _logger.LogError(error, "进程发生了错误。");
+
+        private void OnNext(FormattedEvent formatted) =>
+            _logger.LogInformation(
+                $"[{formatted.Target}]{formatted.Progress}%|已用：{formatted.UsedTime}|剩余：{formatted.RemainingTime}|");
+
+        #endregion
 
         private readonly CommandLineHelper _commandLineHelper;
         private readonly PluginHelper _pluginHelper;
+        private readonly FormattingHelper _formattingHelper;
         private readonly ILogger<ProcessRunner> _logger;
+
+        public void Dispose()
+        {
+            _unsubscribe?.Dispose();
+        }
     }
 }
