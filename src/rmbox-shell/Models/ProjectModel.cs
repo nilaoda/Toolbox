b2bd2bd2af6d4bfc50792bd76840370d142121da
Il Harper <afanyiyu@hotmail.com>
2021-02-18 15:20:04 +0800

    feat: Add ProjectModel

---
+++ src/rmbox-shell/Models/ProjectModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using Ruminoid.Toolbox.Core;
+
+namespace Ruminoid.Toolbox.Shell.Models
+{
+    public class ProjectModel
+    {
+        #region Constructor
+
+        public ProjectModel(OperationModel operationModel, Collection<Tuple<ConfigSectionAttribute, object>> configSections)
+        {
+            _operationModel = operationModel;
+            _configSections = configSections;
+
+            Tuple<ConfigSectionAttribute, object>
+                ioConfigSection =
+                    _configSections
+                        .FirstOrDefault(
+                            x => x.Item1.Id ==
+                                 "Ruminoid.Toolbox.Plugins.Common.ConfigSections.IOConfigSection");
+
+            // ReSharper disable once InvertIf
+            if (ioConfigSection is not null)
+            {
+                JObject jObject = JObject.FromObject(ioConfigSection.Item2);
+
+                var source = jObject["video"]?.ToString();
+                if (string.IsNullOrWhiteSpace(source)) source = jObject["subtitle"]?.ToString();
+                if (!string.IsNullOrWhiteSpace(source)) Source = source;
+            }
+        }
+
+        #endregion
+
+        #region Fields
+
+        private readonly OperationModel _operationModel;
+
+        private readonly Collection<Tuple<ConfigSectionAttribute, object>> _configSections;
+
+        #endregion
+
+        #region Properties
+
+        public string OperationName => _operationModel.Name;
+
+        public string Source { get; } = "";
+
+        #endregion
+    }
+}
