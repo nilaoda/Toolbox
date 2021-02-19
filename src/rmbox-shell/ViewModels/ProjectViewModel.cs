2728802b6464fad5164e8ca8c76aa6a956f6e35d
Il Harper <afanyiyu@hotmail.com>
2021-02-19 13:20:35 +0800

    refactor: ProjectViewModel Update

--- src/rmbox-shell/ViewModels/ProjectViewModel.cs
+++ src/rmbox-shell/ViewModels/ProjectViewModel.cs
@@ -1,4 +1,9 @@
-﻿using ReactiveUI;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using ReactiveUI;
+using Ruminoid.Toolbox.Core;
 using Ruminoid.Toolbox.Shell.Models;
 
 namespace Ruminoid.Toolbox.Shell.ViewModels
@@ -8,16 +13,47 @@ namespace Ruminoid.Toolbox.Shell.ViewModels
         #region Constructor
 
         public ProjectViewModel(
-            ProjectModel projectModel)
+            OperationModel operationModel,
+            Collection<Tuple<ConfigSectionAttribute, object>> configSections)
         {
-            ProjectModel = projectModel;
+            _operationModel = operationModel;
+            _configSections = configSections;
+
+            Tuple<ConfigSectionAttribute, object>
+                ioConfigSection =
+                    _configSections
+                        .FirstOrDefault(
+                            x => x.Item1.Id ==
+                                 "Ruminoid.Toolbox.Plugins.Common.ConfigSections.IOConfigSection");
+
+            // ReSharper disable once InvertIf
+            if (ioConfigSection is not null)
+            {
+                JObject jObject = JObject.FromObject(ioConfigSection.Item2);
+
+                var source = jObject["video"]?.ToString();
+                if (string.IsNullOrWhiteSpace(source)) source = jObject["subtitle"]?.ToString();
+                if (!string.IsNullOrWhiteSpace(source)) Source = source;
+            }
         }
 
         #endregion
 
-        #region ProjectModel Data
+        #region Fields
 
-        public ProjectModel ProjectModel { get; }
+        private readonly OperationModel _operationModel;
+
+        private readonly Collection<Tuple<ConfigSectionAttribute, object>> _configSections;
+
+        #endregion
+
+        #region Properties
+
+        public string OperationName => _operationModel.Name;
+
+        public string Source { get; } = "";
+
+        public Guid Id { get; } = new();
 
         #endregion
 
