4d7b7a4cb0bca0a455eac57cf8dfabfe54f0c859
Il Harper <afanyiyu@hotmail.com>
2021-02-19 14:09:47 +0800

    fix: QueueView Bugfix

--- src/rmbox-shell/Views/QueueView.axaml.cs
+++ src/rmbox-shell/Views/QueueView.axaml.cs
@@ -1,7 +1,7 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
-namespace Ruminoid.Toolbox.Shell.Views.Views
+namespace Ruminoid.Toolbox.Shell.Views
 {
     public class QueueView : UserControl
     {
