31f792b71251971f10897189a931d23885878b61
Il Harper <afanyiyu@hotmail.com>
2021-02-27 01:55:16 +0800

    feat: Add Plugin Meta

---
+++ plugins/rmbox-plugin-mp4box/Mp4BoxMeta.cs
@@ -0,0 +1,11 @@
+﻿using Ruminoid.Toolbox.Core;
+
+namespace Ruminoid.Toolbox.Plugins.Mp4Box
+{
+    [RmboxPlugin]
+    public class Mp4BoxMeta : IMeta
+    {
+        public string Name => "小丸（Mp4Box）";
+        public string Description => "提供了小丸压制法等小丸工具箱功能。";
+    }
+}
---
+++ plugins/rmbox-plugin-x264/X264Meta.cs
@@ -0,0 +1,11 @@
+﻿using Ruminoid.Toolbox.Core;
+
+namespace Ruminoid.Toolbox.Plugins.X264
+{
+    [RmboxPlugin]
+    public class X264Meta : IMeta
+    {
+        public string Name => "X264";
+        public string Description => "提供了 X264 的基础功能和格式器。";
+    }
+}
