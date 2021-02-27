3df151f9d5ffb1d46bbedc6e7e98376f1bff270c
Il Harper <afanyiyu@hotmail.com>
2021-02-27 13:39:40 +0800

    feat: Add PathExtension

---
+++ src/rmbox-plugbase/Utils/Extensions/PathExtension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ruminoid.Toolbox.Utils.Extensions
+{
+    public static class PathExtension
+    {
+        public static string GetMidString(
+            this string source,
+            string before,
+            string after)
+        {
+            var result = string.Empty;
+            try
+            {
+                var startIndex = source.IndexOf(before, StringComparison.Ordinal);
+
+                if (startIndex == -1)
+                    return result;
+
+                var substring = source.Substring(startIndex + before.Length);
+
+                var endIndex = substring.IndexOf(after, StringComparison.Ordinal);
+
+                if (endIndex == -1)
+                    return result;
+
+                result = substring.Remove(endIndex);
+            }
+            catch (Exception)
+            {
+                // Ignore
+            }
+
+            return result;
+        }
+    }
+}
