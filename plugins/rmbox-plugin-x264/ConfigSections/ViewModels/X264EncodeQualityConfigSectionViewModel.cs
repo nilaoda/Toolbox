a7bc47fa47361d09057e22ded9d72dd8db3d2e26
Il Harper <afanyiyu@hotmail.com>
2021-02-27 16:22:21 +0800

    feat: Add X264EncodeQualityConfigSection

---
+++ plugins/rmbox-plugin-x264/ConfigSections/ViewModels/X264EncodeQualityConfigSectionViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using ReactiveUI;
+
+namespace Ruminoid.Toolbox.Plugins.X264.ConfigSections.ViewModels
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class X264EncodeQualityConfigSectionViewModel : ReactiveObject
+    {
+        #region Data
+
+        [JsonProperty("encode_mode")]
+        private string _encodeMode = "crf";
+
+        public string EncodeMode
+        {
+            get => _encodeMode;
+            set => this.RaiseAndSetIfChanged(ref _encodeMode, value);
+        }
+
+        [JsonProperty("crf_value")]
+        private double _crfValue = 21;
+
+        public double CrfValue
+        {
+            get => _crfValue;
+            set => this.RaiseAndSetIfChanged(ref _crfValue, value);
+        }
+
+        [JsonProperty("2pass_value")]
+        private double _twoPassValue = 5000;
+
+        public double TwoPassValue
+        {
+            get => _twoPassValue;
+            set => this.RaiseAndSetIfChanged(ref _twoPassValue, value);
+        }
+
+        #endregion
+
+        #region Commands
+
+        public void ApplyCrfPreset(double value) => CrfValue = value;
+
+        public void ApplyTwoPassPreset(double value) => TwoPassValue = value;
+
+        #endregion
+    }
+}
---
+++ plugins/rmbox-plugin-x264/ConfigSections/Views/X264EncodeQualityConfigSection.axaml.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia.Markup.Xaml;
+using Ruminoid.Toolbox.Core;
+using Ruminoid.Toolbox.Plugins.X264.ConfigSections.ViewModels;
+
+namespace Ruminoid.Toolbox.Plugins.X264.ConfigSections.Views
+{
+    [ConfigSection(
+        "Ruminoid.Toolbox.Plugins.X264.ConfigSections.X264EncodeQualityConfigSection",
+        "视频质量")]
+    public class X264EncodeQualityConfigSection : ConfigSectionBase
+    {
+        public X264EncodeQualityConfigSection()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        #region Data
+
+        public override X264EncodeQualityConfigSectionViewModel Config => DataContext as X264EncodeQualityConfigSectionViewModel;
+
+        #endregion
+    }
+}
