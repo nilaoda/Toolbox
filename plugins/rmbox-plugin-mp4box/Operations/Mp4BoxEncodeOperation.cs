12fa1a5707465cd299a7fbc6f19582bc06d943bc
Il Harper <afanyiyu@hotmail.com>
2021-02-27 17:17:14 +0800

    feat: Add Mp4BoxEncodeOperation

--- plugins/rmbox-plugin-mp4box/Operations/Mp4BoxEncodeOperation.cs
+++ plugins/rmbox-plugin-mp4box/Operations/Mp4BoxEncodeOperation.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json.Linq;
 using Ruminoid.Toolbox.Core;
+using Ruminoid.Toolbox.Utils.Extensions;
 
 namespace Ruminoid.Toolbox.Plugins.Mp4Box.Operations
 {
@@ -13,16 +15,70 @@ namespace Ruminoid.Toolbox.Plugins.Mp4Box.Operations
     {
         public List<Tuple<string, string>> Generate(Dictionary<string, JToken> sectionData)
         {
-            JToken ioSection = sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.IOConfigSection"];
+            JToken ioSection =
+                sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.IOConfigSection"];
+            JToken x264QualitySection =
+                sectionData["Ruminoid.Toolbox.Plugins.X264.ConfigSections.X264EncodeQualityConfigSection"];
 
-            return new List<Tuple<string, string>>
+            string videoPath = Path.GetFullPath(ioSection["video"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string outputPath = Path.GetFullPath(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string atempPath = Path.ChangeExtension(videoPath, "atemp.aac").EscapePathStringForArg();
+            string vtempPath = Path.ChangeExtension(videoPath, "vtemp.mp4").EscapePathStringForArg();
+            string vtempStatsPath = Path.ChangeExtension(videoPath, "vtemp.stats").EscapePathStringForArg();
+
+            List<Tuple<string, string>> result = new()
             {
+                // Extract Audio
+                new(
+                    "ffmpeg",
+                    $"-i {videoPath} -vn -sn -c:a copy -y -map 0:a:0 {atempPath}")
             };
+
+            switch (x264QualitySection["encode_mode"]?.ToObject<string>())
+            {
+                case "crf":
+                    result.AddRange(new Tuple<string, string>[]
+                    {
+                        new(
+                            "x264",
+                            $"--crf {x264QualitySection["crf_value"]?.ToObject<double>():N1} --preset 8  -I 300 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 -o {vtempPath} {videoPath}"),
+                        new(
+                            "ffmpeg",
+                            $"-i {vtempPath} -i {atempPath} -vcodec copy -acodec copy {outputPath}"),
+                        new(
+                            "pwsh",
+                            $"-Command Remove-Item {atempPath} {vtempPath}")
+                    });
+                    break;
+                case "2pass":
+                    result.AddRange(new Tuple<string, string>[]
+                    {
+                        new(
+                            "x264",
+                            $"--pass 1 --bitrate {x264QualitySection["2pass_value"]?.ToObject<int>()} --stats {vtempStatsPath} --preset 8  -I 300 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 -o NUL {videoPath}"),
+                        new(
+                            "x264",
+                            $"--pass 2 --bitrate {x264QualitySection["2pass_value"]?.ToObject<int>()} --stats {vtempStatsPath} --preset 8  -I 300 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8 -o {vtempPath} {videoPath}"),
+                        new(
+                            "ffmpeg",
+                            $"-i {vtempPath} -i {atempPath} -vcodec copy -acodec copy {outputPath}"),
+                        new(
+                            "pwsh",
+                            $"-Command Remove-Item {atempPath} {vtempPath}")
+                    });
+                    break;
+                default:
+                    // ReSharper disable once NotResolvedInText
+                    throw new ArgumentOutOfRangeException("encode_mode");
+            }
+
+            return result;
         }
 
         public List<string> RequiredConfigSections => new()
         {
-            "Ruminoid.Toolbox.Plugins.Common.ConfigSections.IOConfigSection"
+            "Ruminoid.Toolbox.Plugins.Common.ConfigSections.IOConfigSection",
+            "Ruminoid.Toolbox.Plugins.X264.ConfigSections.X264EncodeQualityConfigSection"
         };
     }
 }
