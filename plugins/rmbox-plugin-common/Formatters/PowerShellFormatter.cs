c51de0ff4db9e811838f021446c0c5951ef95ae9
Il Harper <afanyiyu@hotmail.com>
2021-02-27 17:25:57 +0800

    feat: Add PowerShellFormatter

---
+++ plugins/rmbox-plugin-common/Formatters/PowerShellFormatter.cs
@@ -0,0 +1,13 @@
+﻿using Ruminoid.Toolbox.Formatting;
+
+namespace Ruminoid.Toolbox.Plugins.Common.Formatters
+{
+    [Formatter("pwsh")]
+    public class PowerShellFormatter : IFormatter
+    {
+        public FormattedEvent Format(string target, string data)
+        {
+            return new(target, 0, true, "正在运行脚本", data);
+        }
+    }
+}
