f7ffd210c1e798db0ac873a89684e56dbf107f9b
Il Harper <afanyiyu@hotmail.com>
2021-03-06 15:46:00 +0800

    feat: Add RoslimException

---
+++ src/rmbox/Composition/Roslim/RoslimException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ruminoid.Toolbox.Composition.Roslim
+{
+    [Serializable]
+    public class RoslimException : Exception
+    {
+        public RoslimException()
+        {
+        }
+
+        public RoslimException(string message) : base(message)
+        {
+        }
+
+        public RoslimException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
