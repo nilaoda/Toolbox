e2b4646194609289a86ab043092cb98cced58a67
Il Harper <afanyiyu@hotmail.com>
2021-03-08 20:38:53 +0800

    feat: Add CommandHelper

---
+++ src/rmbox-shell/Helpers/CommandHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Reactive;
+using Avalonia.Controls;
+using ReactiveUI;
+
+namespace Ruminoid.Toolbox.Shell.Helpers
+{
+    public static class CommandHelper
+    {
+        public static ReactiveCommand<Button, Unit> OpenContextMenuCommand =>
+            ReactiveCommand.Create<Button>(button =>
+            {
+                if (button?.ContextMenu is null)
+                    return;
+                ContextMenu menu = button.ContextMenu;
+                menu.PlacementMode = PlacementMode.Bottom;
+                menu.PlacementTarget = button;
+                menu.Open();
+            });
+    }
+}
