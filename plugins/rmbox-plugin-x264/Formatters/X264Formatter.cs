b0be050b2a2538675f321d81407aca540308ae2c
Il Harper <afanyiyu@hotmail.com>
2021-03-14 00:40:07 +0800

    feat: Use F1 in X264Formatter

--- plugins/rmbox-plugin-x264/Formatters/X264Formatter.cs
+++ plugins/rmbox-plugin-x264/Formatters/X264Formatter.cs
@@ -24,7 +24,7 @@ namespace Ruminoid.Toolbox.Plugins.X264.Formatters
 
             if (parseProgress)
             {
-                summaryBuilder.Append(progress);
+                summaryBuilder.Append(progress.ToString("F1"));
                 summaryBuilder.Append("% - ");
             }
 
