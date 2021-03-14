97b9e7588f96ba1fec307849e01662cc95892616
Il Harper <afanyiyu@hotmail.com>
2021-03-14 18:28:14 +0800

    feat: Add HwEncMeta

---
+++ plugins/rmbox-plugin-hwenc/HwEncMeta.cs
@@ -0,0 +1,11 @@
+﻿using Ruminoid.Toolbox.Core;
+
+namespace Ruminoid.Toolbox.Plugins.HwEnc
+{
+    [RmboxPlugin]
+    public class HwEncMeta : IMeta
+    {
+        public string Name => "显卡压制";
+        public string Description => "提供了显卡压制（NVEnc/QSVEnc/VCEEnc）的基础功能、操作和格式器。";
+    }
+}
