050a376f40bbc4eb3edc8236f910ea2644e4b3b4
Il Harper <afanyiyu@hotmail.com>
2021-03-16 22:49:57 +0800

    feat: Add plugin-audio

---
+++ plugins/rmbox-plugin-audio/AudioMeta.cs
@@ -0,0 +1,11 @@
+﻿using Ruminoid.Toolbox.Core;
+
+namespace Ruminoid.Toolbox.Plugins.Audio
+{
+    [RmboxPlugin]
+    public class AudioMeta : IMeta
+    {
+        public string Name => "音频压制";
+        public string Description => "提供了音频压制相关的功能。";
+    }
+}
