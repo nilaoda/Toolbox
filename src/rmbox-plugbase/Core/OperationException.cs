f9dca09db15e4172d85ef8de122869b2f4086f76
Il Harper <afanyiyu@hotmail.com>
2021-03-17 20:20:41 +0800

    feat: Add OperationException

---
+++ src/rmbox-plugbase/Core/OperationException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Ruminoid.Toolbox.Core
+{
+    [Serializable]
+    public class OperationException : Exception
+    {
+        #region Constructor
+
+        public OperationException(
+            string message,
+            Type operationType,
+            Exception inner = null) : base(message, inner)
+        {
+            OperationType = operationType;
+        }
+
+        #endregion
+
+        public Type OperationType { get; }
+
+        public override string ToString()
+        {
+            return $"操作 {OperationType.Name} 引发了异常：\n{base.ToString()}";
+        }
+    }
+}
