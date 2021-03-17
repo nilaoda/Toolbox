86d5217c33209e1f2996027df1970111ee0cc40e
Il Harper <afanyiyu@hotmail.com>
2021-03-17 21:29:08 +0800

    feat: Add VSFM Support for HwEncOperation

--- plugins/rmbox-plugin-hwenc/Operations/HwEncOperation.cs
+++ plugins/rmbox-plugin-hwenc/Operations/HwEncOperation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Newtonsoft.Json.Linq;
 using Ruminoid.Toolbox.Core;
 using Ruminoid.Toolbox.Utils.Extensions;
@@ -32,34 +33,74 @@ namespace Ruminoid.Toolbox.Plugins.HwEnc.Operations
 
             string defaultArgs = "";
 
-            string inputPath = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string inputPathIntl = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty);
+            string inputPath = inputPathIntl.EscapePathStringForArg();
+            string subtitlePathIntl = PathExtension.GetFullPathOrEmpty(ioSection["subtitle"]?.ToObject<string>() ?? string.Empty);
+            string subtitlePath = subtitlePathIntl.EscapePathStringForArg();
             string outputPath = PathExtension.GetFullPathOrEmpty(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string lwiPath = (inputPathIntl + ".lwi").EscapePathStringForArg();
 
-            switch (hwEncQualitySection["encode_mode"]?.ToObject<string>())
+            bool isVpy = inputPathIntl.EndsWith(".vpy");
+            bool isVsfm = !string.IsNullOrEmpty(subtitlePathIntl);
+
+            bool useVpy = isVpy || isVsfm;
+
+            if (isVpy && isVsfm)
+                throw new OperationException(
+                    "不支持在 VapourSynth 输入上使用 VSFilterMod。请在 VapourSynth 中完成字幕处理。",
+                    typeof(HwEncOperation));
+
+            List<(string Target, string Args, string Formatter)> result = new();
+
+            if (isVsfm)
             {
-                case "cqp":
-                    return new List<(string Target, string Args, string Formatter)>
-                    {
-                        (hwEncCore,
-                            $"-i {inputPath} -o {outputPath} --avhw --audio-copy --codec {hwEncCodecSection["codec"]?.ToObject<string>()} --cqp {hwEncQualitySection["cqp_value"]?.ToObject<string>()} {(useCustomArgs ? customArgs : defaultArgs)}",
-                            hwEncCore)
-                    };
-                case "2pass":
-                    return new List<(string Target, string Args, string Formatter)>
-                    {
-                        (hwEncCore,
-                            $"-i {inputPath} -o {outputPath} --avhw --audio-copy --codec {hwEncCodecSection["codec"]?.ToObject<string>()} --vbr {hwEncQualitySection["2pass_value"]?.ToObject<int>()} --multipass 2pass-full {(useCustomArgs ? customArgs : defaultArgs)}",
-                            hwEncCore)
-                    };
-                default:
-                    // ReSharper disable once NotResolvedInText
-                    throw new ArgumentOutOfRangeException("encode_mode");
+                string vpyPath = Path.ChangeExtension(inputPathIntl, "vpy").EscapePathStringForArg();
+
+                result.Add(
+                    ("node",
+                        $"rmbox-vpygen.js {inputPath} {subtitlePath} {vpyPath}",
+                        "null"));
+
+                inputPath = vpyPath;
             }
+
+            string encodeMode = hwEncQualitySection["encode_mode"]?.ToObject<string>() switch
+            {
+                "cqp" => $"--cqp {hwEncQualitySection["cqp_value"]?.ToObject<string>()}",
+                "2pass" => $"--vbr {hwEncQualitySection["2pass_value"]?.ToObject<int>()} --multipass 2pass-full",
+                // ReSharper disable once NotResolvedInText
+                _ => throw new ArgumentOutOfRangeException("encode_mode")
+            };
+
+            result.Add(
+                (hwEncCore,
+                    $"{(useVpy ? "--vpy" : "--avhw")} -i {inputPath} -o {outputPath} --audio-copy --codec {hwEncCodecSection["codec"]?.ToObject<string>()} {encodeMode} {(useCustomArgs ? customArgs : defaultArgs)}",
+                    hwEncCore));
+
+            if (useVpy)
+            {
+                result.Add(
+                    ("pwsh",
+                        "-Command If (Test-Path " + lwiPath + " ) { Remove-Item " + lwiPath + " }",
+                        "null"));
+                result.Add(
+                    ("pwsh",
+                        $"-Command Remove-Item {inputPath}",
+                        "null"));
+            }
+
+            return result;
         }
 
         public Dictionary<string, JToken> RequiredConfigSections => new()
         {
-            {"Ruminoid.Toolbox.Plugins.Common.ConfigSections.IOConfigSection", new JObject()},
+            {
+                "Ruminoid.Toolbox.Plugins.Common.ConfigSections.IOConfigSection", JToken.FromObject(new
+                {
+                    support_subtitle = true,
+                    support_vsfm = true
+                })
+            },
             {"Ruminoid.Toolbox.Plugins.HwEnc.ConfigSections.HwEncCoreConfigSection", new JObject()},
             {"Ruminoid.Toolbox.Plugins.HwEnc.ConfigSections.HwEncCodecConfigSection", new JObject()},
             {"Ruminoid.Toolbox.Plugins.HwEnc.ConfigSections.HwEncQualityConfigSection", new JObject()},
