1b127607c389ddb58f42e211fb136effbaa4fbe5
Il Harper <afanyiyu@hotmail.com>
2021-03-18 23:11:32 +0800

    feat: Add RoslimMeta

---
+++ src/rmbox/Composition/Roslim/RoslimMeta.cs
@@ -0,0 +1,21 @@
+﻿using Ruminoid.Toolbox.Core;
+
+namespace rmbox.Composition.Roslim
+{
+    public class RoslimMeta : IMeta
+    {
+        public RoslimMeta(
+            string name,
+            string description,
+            string author)
+        {
+            Name = name;
+            Description = description;
+            Author = author;
+        }
+
+        public string Name { get; }
+        public string Description { get; }
+        public string Author { get; }
+    }
+}
