f59136a602fff72ade9ea48d1bcbe85fd93efa39
Il Harper <afanyiyu@hotmail.com>
2021-03-20 20:37:04 +0800

    feat: Add GetAllFiles Method

---
+++ src/rmbox-plugbase/Utils/Extensions/GetAllFiles.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace Ruminoid.Toolbox.Utils.Extensions
+{
+    public static partial class PathExtension
+    {
+        public static List<string> GetAllFiles(
+            string directory)
+        {
+            List<string> result = new();
+            GetAllFilesIntl(directory, result);
+            return result;
+        }
+
+        private static void GetAllFilesIntl(
+            string directory,
+            List<string> result)
+        {
+            result.AddRange(Directory.GetFiles(directory));
+            foreach (string d in Directory.GetDirectories(directory)) GetAllFilesIntl(d, result);
+        }
+    }
+}
