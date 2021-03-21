3540340a3aab2f20383afb04931b0006413336c5
Il Harper <afanyiyu@hotmail.com>
2021-03-21 20:03:18 +0800

    feat: Mp4BoxEncodeOperation Update

--- plugins/rmbox-plugin-mp4box/Operations/Mp4BoxEncodeOperation.cs
+++ plugins/rmbox-plugin-mp4box/Operations/Mp4BoxEncodeOperation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 using Ruminoid.Toolbox.Core;
 using Ruminoid.Toolbox.Utils.Extensions;
@@ -15,18 +16,102 @@ namespace Ruminoid.Toolbox.Plugins.Mp4Box.Operations
     {
         public List<(string Target, string Args, string Formatter)> Generate(Dictionary<string, JToken> sectionData)
         {
+            #region 输入/输出
+
             JToken ioSection =
                 sectionData[ConfigSectionBase.IOConfigSectionId];
+
+            #endregion
+            
+            #region 输入文件
+
+            string inputPathIntl = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty);
+            string inputPath = inputPathIntl.EscapePathStringForArg();
+
+            #endregion
+
+            #region 输入字幕
+
+            string subtitlePathIntl = PathExtension.GetFullPathOrEmpty(ioSection["subtitle"]?.ToObject<string>() ?? string.Empty);
+            string subtitlePath = subtitlePathIntl.EscapePathStringForArg();
+
+            #endregion
+
+            #region 字幕相关
+
+            bool isIncludingSubtitle = !string.IsNullOrWhiteSpace(subtitlePathIntl);
+
+            #endregion
+
+            #region VapourSynth 相关
+
+            string lwiPath = (inputPathIntl + ".lwi").EscapePathStringForArg();
+
+            bool isVpy = inputPathIntl.EndsWith(".vpy");
+            bool isVsfm = ioSection["use_vsfm"].ToObject<bool>();
+
+            bool useVpy = isVpy || isVsfm;
+
+            if (isVpy && isVsfm)
+                throw new OperationException(
+                    "不支持在 VapourSynth 输入上使用 VSFilterMod。请在 VapourSynth 中完成字幕处理。",
+                    typeof(Mp4BoxEncodeOperation));
+
+            #endregion
+
+            #region x264 质量
+
             JToken x264QualitySection =
                 sectionData["Ruminoid.Toolbox.Plugins.X264.ConfigSections.X264EncodeQualityConfigSection"];
+
+            #endregion
+
+            #region x264 核心
+
             JToken x264CoreSection =
                 sectionData["Ruminoid.Toolbox.Plugins.X264.ConfigSections.X264CoreConfigSection"];
-            JToken audioSection =
-                sectionData["Ruminoid.Toolbox.Plugins.Audio.ConfigSections.AudioConfigSection"];
-            JToken customArgsSection =
-                sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection"];
+
+            #endregion
+
+            #region x264 相关
+
+            string vtempPath = Path.ChangeExtension(inputPathIntl, "vtemp.mp4").EscapePathStringForArg();
+            string vtempStatsPath = Path.ChangeExtension(inputPathIntl, "vtemp.stats").EscapePathStringForArg();
+            string vtempStatsMbtreePath = Path.ChangeExtension(inputPathIntl, "vtemp.stats.mbtree").EscapePathStringForArg();
+
+            string x264EncodeMode = x264QualitySection["encode_mode"]?.ToObject<string>() ?? string.Empty;
 
             string x264Core = x264CoreSection["core"]?.ToObject<string>();
+
+            #endregion
+
+            #region 音频
+
+            JToken audioSection =
+                sectionData["Ruminoid.Toolbox.Plugins.Audio.ConfigSections.AudioConfigSection"];
+
+            #endregion
+
+            #region 音频相关
+
+            string atempPath = Path.ChangeExtension(inputPathIntl, "atemp.aac").EscapePathStringForArg();
+
+            string audioMode = audioSection["mode"]?.ToObject<string>();
+            bool hasAudio = audioMode != "none";
+            int audioBitrate = audioSection["bitrate"].ToObject<int>();
+
+            #endregion
+
+            #region 输出相关
+
+            string outputPath = PathExtension.GetFullPathOrEmpty(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+
+            #endregion
+
+            #region 自定义参数
+
+            JToken customArgsSection =
+                sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection"];
 
             string customArgs = customArgsSection["args"]?.ToObject<string>();
             bool useCustomArgs = !string.IsNullOrWhiteSpace(customArgs);
@@ -34,23 +119,17 @@ namespace Ruminoid.Toolbox.Plugins.Mp4Box.Operations
             string defaultArgs =
                 @"--preset 8 -I 300 -r 4 -b 3 --me umh -i 1 --scenecut 60 -f 1:1 --qcomp 0.5 --psy-rd 0.3:0 --aq-mode 2 --aq-strength 0.8";
 
-            string audioMode = audioSection["mode"]?.ToObject<string>();
-            bool hasAudio = audioMode != "none";
-            int audioBitrate = audioSection["bitrate"].ToObject<int>();
+            #endregion
 
-            string inputPathIntl = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty);
-            string inputPath = inputPathIntl.EscapePathStringForArg();
-            string subtitlePathIntl = PathExtension.GetFullPathOrEmpty(ioSection["subtitle"]?.ToObject<string>() ?? string.Empty);
-            bool isIncludingSubtitle = !string.IsNullOrWhiteSpace(subtitlePathIntl);
-            string subtitlePath = subtitlePathIntl.EscapePathStringForArg();
-            string outputPath = PathExtension.GetFullPathOrEmpty(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
-            string atempOriginPath = Path.ChangeExtension(inputPathIntl, "atemp.origin.aac").EscapePathStringForArg();
-            string atempPath = Path.ChangeExtension(inputPathIntl, "atemp.aac").EscapePathStringForArg();
-            string vtempPath = Path.ChangeExtension(inputPathIntl, "vtemp.mp4").EscapePathStringForArg();
-            string vtempStatsPath = Path.ChangeExtension(inputPathIntl, "vtemp.stats").EscapePathStringForArg();
-            string vtempStatsMbtreePath = Path.ChangeExtension(inputPathIntl, "vtemp.stats.mbtree").EscapePathStringForArg();
+            #region 准备命令
 
-            List<(string, string, string)> result = new();
+            List<(string Target, string Args, string Formatter)> result = new();
+
+            #endregion
+
+            // 开始处理
+
+            #region 处理音频
 
             switch (audioMode)
             {
@@ -63,84 +142,114 @@ namespace Ruminoid.Toolbox.Plugins.Mp4Box.Operations
                 case "process":
                     result.Add(new(
                         "ffmpeg",
-                        $"-i {inputPath} -vn -sn -c:a copy -y -map 0:a:0 {atempOriginPath}",
-                        "null"));
-                    result.Add(new(
-                        "qaac64",
-                        $"-q 2 --ignorelength -c {audioBitrate} {atempOriginPath} -o {atempPath}",
+                        $"-i {inputPath} -vn -sn -v 0 -c:a pcm_s16le -f wav pipe: | {PathExtension.GetTargetPath("qaac64")} -q 2 --ignorelength -c {audioBitrate} - -o {atempPath}",
                         "null"));
                     break;
             }
 
-            switch (x264QualitySection["encode_mode"]?.ToObject<string>())
+            #endregion
+
+            #region 处理 VSFM
+
+            if (isVsfm)
+            {
+                string vpyPath = Path.ChangeExtension(inputPathIntl, "vpy").EscapePathStringForArg();
+
+                result.Add(
+                    ("node",
+                        $"rmbox-vpygen.js {inputPath} {subtitlePath} {vpyPath}",
+                        "null"));
+
+                inputPath = vpyPath;
+            }
+
+            #endregion
+
+            #region 处理 x264 参数
+
+            string x264Args = $"{(useCustomArgs ? customArgs : defaultArgs)} {(isIncludingSubtitle && !isVsfm ? "--vf subtitles --sub " + subtitlePath : "")}";
+
+            #endregion
+
+            #region 处理视频
+
+            switch (x264EncodeMode)
             {
                 case "crf":
-                    result.Add(new(
-                        x264Core,
-                        $"--crf {x264QualitySection["crf_value"]?.ToObject<double>():N1} {(useCustomArgs ? customArgs : defaultArgs)} {(isIncludingSubtitle ? "--vf subtitles --sub " + subtitlePath : "")} -o {(hasAudio ? vtempPath : outputPath)} {inputPath}",
-                        x264Core));
-                    if (hasAudio)
-                        result.Add(new(
-                            "ffmpeg",
-                            $"-i {vtempPath} -i {atempPath} -vcodec copy -acodec copy {outputPath}",
-                            "ffmpeg"));
-                    result.Add(new(
-                        "pwsh",
-                        $"-Command Remove-Item {vtempPath}",
-                        "null"));
-                    if (hasAudio)
-                        result.Add(new(
-                            "pwsh",
-                            $"-Command Remove-Item {atempPath}",
-                            "null"));
+                    result.Add(
+                        GenerateVideoProcessingCommand(
+                            $"--crf {x264QualitySection["crf_value"]?.ToObject<double>():N1} {x264Args} -o {(hasAudio ? vtempPath : outputPath)}",
+                            x264Core,
+                            inputPath,
+                            useVpy));
                     break;
                 case "2pass":
                     result.AddRange(new (string, string, string)[]
                     {
                         new(
                             x264Core,
-                            $"--pass 1 --bitrate {x264QualitySection["2pass_value"]?.ToObject<int>()} --stats {vtempStatsPath} {(useCustomArgs ? customArgs : defaultArgs)} {(isIncludingSubtitle ? "--vf subtitles --sub " + subtitlePath : "")} -o NUL {inputPath}",
+                            $"--pass 1 --bitrate {x264QualitySection["2pass_value"]?.ToObject<int>()} --stats {vtempStatsPath} {x264Args} -o NUL",
                             x264Core),
                         new(
                             x264Core,
-                            $"--pass 2 --bitrate {x264QualitySection["2pass_value"]?.ToObject<int>()} --stats {vtempStatsPath} {(useCustomArgs ? customArgs : defaultArgs)} {(isIncludingSubtitle ? "--vf subtitles --sub " + subtitlePath : "")} -o {(hasAudio ? vtempPath : outputPath)} {inputPath}",
+                            $"--pass 2 --bitrate {x264QualitySection["2pass_value"]?.ToObject<int>()} --stats {vtempStatsPath} {x264Args} -o {(hasAudio ? vtempPath : outputPath)}",
                             x264Core)
                     });
-                    if (hasAudio)
-                        result.Add(new(
-                            "ffmpeg",
-                            $"-i {vtempPath} -i {atempPath} -vcodec copy -acodec copy {outputPath}",
-                            "ffmpeg"));
-                    result.Add(new(
-                        "pwsh",
-                        $"-Command Remove-Item {vtempPath},{vtempStatsPath},{vtempStatsMbtreePath}",
-                        "null"));
-                    if (hasAudio)
-                        result.Add(new(
-                            "pwsh",
-                            $"-Command Remove-Item {atempPath}",
-                            "null"));
                     break;
                 default:
                     // ReSharper disable once NotResolvedInText
                     throw new ArgumentOutOfRangeException("encode_mode");
             }
 
-            if (audioMode == "process")
+            #endregion
+
+            #region 混流
+
+            if (hasAudio)
                 result.Add(new(
-                    "pwsh",
-                    $"-Command Remove-Item {atempOriginPath}",
-                    "null"));
+                    "ffmpeg",
+                    $"-i {vtempPath} -i {atempPath} -vcodec copy -acodec copy {outputPath}",
+                    "ffmpeg"));
+
+            #endregion
+
+            #region 清理临时文件
+
+            result.AddRange(new[]
+                {
+                    vtempPath, atempPath, vtempStatsPath, vtempStatsMbtreePath, lwiPath
+                }
+                .Select(GenerateTryDeleteCommand)
+                .ToList());
+
+            #endregion
 
             return result;
         }
+
+        public static (string Target, string Args, string Formatter) GenerateVideoProcessingCommand(
+            string args,
+            string x264Core,
+            string inputPath,
+            bool useVpy) =>
+            new(
+                useVpy ? "VSPipe" : x264Core,
+                (useVpy ? $"--y4m {inputPath} - | {PathExtension.GetTargetPath(x264Core)} --demuxer y4m - " : "") + args + (useVpy ? "" : $" {inputPath}"),
+                x264Core);
+
+        public static (string Target, string Args, string Formatter) GenerateTryDeleteCommand(
+            string path) =>
+            new("pwsh",
+                "-Command If (Test-Path " + path + " ) { Remove-Item " + path + " }",
+                "null");
 
         public Dictionary<string, JToken> RequiredConfigSections => new()
         {
             {
                 ConfigSectionBase.IOConfigSectionId, JToken.FromObject(new
                 {
-                    support_subtitle = true
+                    support_subtitle = true,
+                    support_vsfm = true
                 })
             },
             {"Ruminoid.Toolbox.Plugins.X264.ConfigSections.X264CoreConfigSection", new JObject()},
