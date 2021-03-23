f7211de895bd3d6ff5837b1e8f6c600e726eee23
Il Harper <afanyiyu@hotmail.com>
2021-03-23 13:29:24 +0800

    fix: Add Try-Catch Block in OperationWindow

--- src/rmbox-shell/Operations/ViewModels/BatchOperationWindowViewModel.cs
+++ src/rmbox-shell/Operations/ViewModels/BatchOperationWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Linq;
 using Avalonia.Controls;
 using DynamicData;
 using Newtonsoft.Json.Linq;
@@ -55,52 +56,66 @@ namespace Ruminoid.Toolbox.Shell.Operations.ViewModels
 
         private void InitializeTabs()
         {
-            ConfigSectionAttribute batchAttribute =
-                Attribute.GetCustomAttribute(typeof(BatchIOConfigSection), typeof(ConfigSectionAttribute)) as
-                    ConfigSectionAttribute;
-            BatchIOConfigSection batchConfig = new BatchIOConfigSection();
-
-            _configSections.Add((batchAttribute, batchConfig));
-
-            Items.Add(
-                new()
-                {
-                    Header = batchAttribute?.Name,
-                    Content = batchConfig
-                });
-
-            _operation = Activator.CreateInstance(OperationModel.Type) as IOperation;
-
-            if (_operation is null)
-                throw new ArgumentException("Cannot Construct Operation.");
-
-            foreach (KeyValuePair<string, JToken> sectionData in _operation.RequiredConfigSections)
+            try
             {
-                // Exclude
-                if (sectionData.Key == ConfigSectionBase.IOConfigSectionId)
-                    continue;
+                ConfigSectionAttribute batchAttribute =
+                    Attribute.GetCustomAttribute(typeof(BatchIOConfigSection), typeof(ConfigSectionAttribute)) as
+                        ConfigSectionAttribute;
+                BatchIOConfigSection batchConfig = new BatchIOConfigSection();
 
-                (ConfigSectionAttribute ConfigSectionAttribute, Type ConfigSectionType) sectionMeta = _pluginHelper.ConfigSectionCollection
-                    .Where(x => x.ConfigSectionAttribute.Id == sectionData.Key)
-                    .ToArray()
-                    .FirstOrDefault();
-
-                if (sectionMeta == default)
-                    throw new ArgumentException("Cannot Find ConfigSection.");
-
-                ConfigSectionBase section = Activator.CreateInstance(sectionMeta.ConfigSectionType, sectionData.Value) as ConfigSectionBase;
-
-                if (section is null)
-                    throw new ArgumentException("Cannot Construct ConfigSection.");
-
-                _configSections.Add((sectionMeta.ConfigSectionAttribute, section));
+                _configSections.Add((batchAttribute, batchConfig));
 
                 Items.Add(
-                    new TabItem
+                    new()
                     {
-                        Header = sectionMeta.ConfigSectionAttribute.Name,
-                        Content = section
+                        Header = batchAttribute?.Name,
+                        Content = batchConfig
                     });
+
+                _operation = Activator.CreateInstance(OperationModel.Type) as IOperation;
+
+                if (_operation is null)
+                    throw new ArgumentException("Cannot Construct Operation.");
+
+                foreach (KeyValuePair<string, JToken> sectionData in _operation.RequiredConfigSections)
+                {
+                    // Exclude
+                    if (sectionData.Key == ConfigSectionBase.IOConfigSectionId)
+                        continue;
+
+                    (ConfigSectionAttribute ConfigSectionAttribute, Type ConfigSectionType) sectionMeta = _pluginHelper.ConfigSectionCollection
+                        .Where(x => x.ConfigSectionAttribute.Id == sectionData.Key)
+                        .ToArray()
+                        .FirstOrDefault();
+
+                    if (sectionMeta == default)
+                        throw new ArgumentException("Cannot Find ConfigSection.");
+
+                    ConfigSectionBase section = Activator.CreateInstance(sectionMeta.ConfigSectionType, sectionData.Value) as ConfigSectionBase;
+
+                    if (section is null)
+                        throw new ArgumentException("Cannot Construct ConfigSection.");
+
+                    _configSections.Add((sectionMeta.ConfigSectionAttribute, section));
+
+                    Items.Add(
+                        new TabItem
+                        {
+                            Header = sectionMeta.ConfigSectionAttribute.Name,
+                            Content = section
+                        });
+                }
+            }
+            catch (Exception)
+            {
+                Observable.FromAsync(
+                        () => MessageBox.ShowAndGetResult(
+                            "警告",
+                            "插件加载出现错误，请检查是否安装了所需的插件。操作可能出现不正常的行为。",
+                            _window,
+                            false),
+                        RxApp.MainThreadScheduler)
+                    .Subscribe(_ => { });
             }
         }
 
--- src/rmbox-shell/Operations/ViewModels/SingleOperationWindowViewModel.cs
+++ src/rmbox-shell/Operations/ViewModels/SingleOperationWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive.Linq;
 using Avalonia.Controls;
 using DynamicData;
 using Newtonsoft.Json.Linq;
@@ -11,6 +12,7 @@ using Ruminoid.Toolbox.Core;
 using Ruminoid.Toolbox.Shell.Models;
 using Ruminoid.Toolbox.Shell.Operations.Views;
 using Ruminoid.Toolbox.Shell.Services;
+using Ruminoid.Toolbox.Shell.Utils.Dialogs;
 using Ruminoid.Toolbox.Shell.ViewModels.Project;
 using Splat;
 
@@ -53,34 +55,48 @@ namespace Ruminoid.Toolbox.Shell.Operations.ViewModels
 
         private void InitializeTabs()
         {
-            _operation = Activator.CreateInstance(OperationModel.Type) as IOperation;
-
-            if (_operation is null)
-                throw new ArgumentException("Cannot Construct Operation.");
-
-            foreach (KeyValuePair<string, JToken> sectionData in _operation.RequiredConfigSections)
+            try
             {
-                (ConfigSectionAttribute ConfigSectionAttribute, Type ConfigSectionType) sectionMeta = _pluginHelper.ConfigSectionCollection
-                    .Where(x => x.ConfigSectionAttribute.Id == sectionData.Key)
-                    .ToArray()
-                    .FirstOrDefault();
+                _operation = Activator.CreateInstance(OperationModel.Type) as IOperation;
 
-                if (sectionMeta == default)
-                    throw new ArgumentException("Cannot Find ConfigSection.");
+                if (_operation is null)
+                    throw new ArgumentException("Cannot Construct Operation.");
 
-                ConfigSectionBase section = Activator.CreateInstance(sectionMeta.ConfigSectionType, sectionData.Value) as ConfigSectionBase;
+                foreach (KeyValuePair<string, JToken> sectionData in _operation.RequiredConfigSections)
+                {
+                    (ConfigSectionAttribute ConfigSectionAttribute, Type ConfigSectionType) sectionMeta = _pluginHelper.ConfigSectionCollection
+                        .Where(x => x.ConfigSectionAttribute.Id == sectionData.Key)
+                        .ToArray()
+                        .FirstOrDefault();
 
-                if (section is null)
-                    throw new ArgumentException("Cannot Construct ConfigSection.");
+                    if (sectionMeta == default)
+                        throw new ArgumentException("Cannot Find ConfigSection.");
 
-                _configSections.Add((sectionMeta.ConfigSectionAttribute, section));
+                    ConfigSectionBase section = Activator.CreateInstance(sectionMeta.ConfigSectionType, sectionData.Value) as ConfigSectionBase;
 
-                Items.Add(
-                    new TabItem
-                    {
-                        Header = sectionMeta.ConfigSectionAttribute.Name,
-                        Content = section
-                    });
+                    if (section is null)
+                        throw new ArgumentException("Cannot Construct ConfigSection.");
+
+                    _configSections.Add((sectionMeta.ConfigSectionAttribute, section));
+
+                    Items.Add(
+                        new TabItem
+                        {
+                            Header = sectionMeta.ConfigSectionAttribute.Name,
+                            Content = section
+                        });
+                }
+            }
+            catch (Exception)
+            {
+                Observable.FromAsync(
+                        () => MessageBox.ShowAndGetResult(
+                            "警告",
+                            "插件加载出现错误，请检查是否安装了所需的插件。操作可能出现不正常的行为。",
+                            _window,
+                            false),
+                        RxApp.MainThreadScheduler)
+                    .Subscribe(_ => { });
             }
         }
 
