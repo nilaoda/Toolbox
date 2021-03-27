bd5e67aa5d5d3416bbf199482f3fd682ad4fd066
Il Harper <afanyiyu@hotmail.com>
2021-03-27 20:02:07 +0800

    feat: Add FFmpegMetaRotateOperation

---
+++ plugins/rmbox-plugin-ffmpeg/Operations/FFmpegMetaRotateOperation.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Ruminoid.Toolbox.Core;
+using Ruminoid.Toolbox.Utils.Extensions;
+
+namespace Ruminoid.Toolbox.Plugins.FFmpeg.Operations
+{
+    [Operation(
+        "Ruminoid.Toolbox.Plugins.FFmpeg.Operations.FFmpegMetaRotateOperation",
+        "画面方向纠正",
+        "纠正由于压制导致错误显示的画面方向（180°）。")]
+    public class FFmpegMetaRotateOperation : IOperation
+    {
+        public List<(string Target, string Args, string Formatter)> Generate(Dictionary<string, JToken> sectionData)
+        {
+            JToken ioSection =
+                sectionData[ConfigSectionBase.IOConfigSectionId];
+
+            string inputPath = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string outputPath = PathExtension.GetFullPathOrEmpty(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+
+            #region 自定义参数
+
+            JToken customArgsSection =
+                sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection"];
+
+            string customArgs = customArgsSection["args"]?.ToObject<string>();
+            bool useCustomArgs = !string.IsNullOrWhiteSpace(customArgs);
+
+            string defaultArgs = "";
+
+            #endregion
+
+            return new List<(string, string, string)>
+            {
+                new(
+                    "ffmpeg",
+                    $"-i {inputPath} -y -c copy -metadata:s:v:0 rotate=180 {(useCustomArgs ? customArgs : defaultArgs)} {outputPath}",
+                    "ffmpeg")
+            };
+        }
+
+        public Dictionary<string, JToken> RequiredConfigSections => new()
+        {
+            {ConfigSectionBase.IOConfigSectionId, new JObject()},
+            {"Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection", new JObject()}
+        };
+    }
+}
