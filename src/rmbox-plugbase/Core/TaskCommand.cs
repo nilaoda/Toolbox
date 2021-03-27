a346de6e4d439e71c5f8d7b44e72ec7660e879b8
Il Harper <afanyiyu@hotmail.com>
2021-03-27 14:01:31 +0800

    feat: Add TaskCommand record

---
+++ src/rmbox-plugbase/Core/TaskCommand.cs
@@ -0,0 +1,15 @@
+﻿namespace Ruminoid.Toolbox.Core
+{
+    public record TaskCommand
+    {
+        public string Target = "";
+        public string Args = "";
+        public string Formatter = "";
+
+        public static implicit operator (string Target, string Args, string Formatter)(TaskCommand command) =>
+            (command.Target, command.Args, command.Formatter);
+
+        public static implicit operator TaskCommand((string Target, string Args, string Formatter) command) =>
+            new() {Target = command.Target, Args = command.Args, Formatter = command.Formatter};
+    }
+}
