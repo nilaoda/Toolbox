29abda4c3455efe9634467a6bbe396fde5008b0b
Il Harper <afanyiyu@hotmail.com>
2021-03-31 22:44:15 +0800

    ci: Add Build Mac Pack Job

---
+++ build/PackInstaller.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.Git;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.GitVersion;
+using Nuke.Common.Utilities.Collections;
+using static Nuke.Common.EnvironmentInfo;
+using static Nuke.Common.IO.FileSystemTasks;
+using static Nuke.Common.IO.PathConstruction;
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+partial class Build : NukeBuild
+{
+    Target PackMac => _ => _
+        .Executes(() =>
+        {
+            AbsolutePath MacDirectory = DistDirectory / "RuminoidToolbox.app";
+            AbsolutePath MacContentsDirectory = MacDirectory / "Contents";
+            AbsolutePath MacMacOSDirectory = MacDirectory / "MacOS";
+            AbsolutePath MacResourcesDirectory = MacDirectory / "Resources";
+
+            new[]
+            {
+                MacDirectory,
+                MacContentsDirectory,
+                MacMacOSDirectory,
+                MacResourcesDirectory
+            }.ForEach(x => EnsureCleanDirectory(x));
+
+            Logger.Info("Copying contents.");
+            ForceCopyDirectoryRecursively(
+                PackDirectory,
+                MacMacOSDirectory);
+
+            Logger.Info("Copying icon.");
+            CopyFileToDirectory(
+                RootDirectory / "common" / "Assets" / "rmbox.icns",
+                MacResourcesDirectory,
+                FileExistsPolicy.Overwrite);
+
+            new Plist
+            {
+                CFBundleName = "Toolbox",
+                CFBundleDisplayName = "Ruminoid Toolbox",
+                CFBundleSpokenName = "Ruminoid Toolbox",
+                CFBundleIdentifier = "world.ruminoid.toolbox",
+                LSApplicationCategoryType = "public.app-category.developer-tools",
+                CFBundleVersion = GitVersion.MajorMinorPatch,
+                CFBundleExecutable = "rmbenv",
+                CFBundleIconFileName = "rmbox.icns",
+                CFBundleShortVersionString = GitVersion.MajorMinorPatch
+            }.Write(MacContentsDirectory);
+
+            Logger.Info("Configuring privileges.");
+            ProcessTasks.StartShell(
+                    "chmod -R 775 RuminoidToolbox.app",
+                    DistDirectory)
+                .AssertZeroExitCode();
+        });
+}
---
+++ build/PlistWriter.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using Nuke.Common;
+
+public class Plist
+{
+    #region Data
+
+    public string CFBundleName { get; init; }
+
+    public string CFBundleDisplayName { get; init; }
+
+    public string CFBundleSpokenName { get; init; }
+
+    public string LSApplicationCategoryType { get; init; }
+
+    public string CFBundleIdentifier { get; init; }
+
+    public string CFBundleVersion { get; init; }
+
+    public string CFBundleExecutable { get; init; }
+
+    public string CFBundleIconFileName { get; init; }
+
+    public string CFBundleShortVersionString { get; init; }
+
+    #endregion
+
+    #region Writer
+
+    public void Write(string contentsDirectory)
+    {
+        var settings = new XmlWriterSettings
+        {
+            Indent = true,
+            NewLineOnAttributes = false
+        };
+
+        var path = Path.Combine(contentsDirectory, "Info.plist");
+
+        Logger.Info($"Writing property list file: {path}");
+        using var xmlWriter = XmlWriter.Create(path, settings);
+
+        xmlWriter.WriteStartDocument();
+
+        xmlWriter.WriteRaw(Environment.NewLine);
+        xmlWriter.WriteRaw(
+            "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">");
+        xmlWriter.WriteRaw(Environment.NewLine);
+
+        xmlWriter.WriteStartElement("plist");
+        xmlWriter.WriteAttributeString("version", "1.0");
+        xmlWriter.WriteStartElement("dict");
+
+        WriteProperty(xmlWriter, nameof(CFBundleName), CFBundleName);
+        WriteProperty(xmlWriter, nameof(CFBundleDisplayName), CFBundleDisplayName);
+        WriteProperty(xmlWriter, nameof(CFBundleSpokenName), CFBundleSpokenName);
+        WriteProperty(xmlWriter, nameof(CFBundleIdentifier), CFBundleIdentifier);
+        WriteProperty(xmlWriter, nameof(CFBundleVersion), CFBundleVersion);
+        WriteProperty(xmlWriter, "CFBundlePackageType", "APPL");
+        //WriteProperty(xmlWriter, nameof(CFBundleSignature), CFBundleSignature);
+        WriteProperty(xmlWriter, nameof(CFBundleExecutable), CFBundleExecutable);
+        WriteProperty(xmlWriter, nameof(CFBundleIconFileName), CFBundleIconFileName);
+        WriteProperty(xmlWriter, nameof(CFBundleShortVersionString), CFBundleShortVersionString);
+        WriteProperty(xmlWriter, nameof(LSApplicationCategoryType), LSApplicationCategoryType);
+        WriteProperty(xmlWriter, "NSPrincipalClass", "NSApplication");
+        WriteProperty(xmlWriter, "NSHumanReadableCopyright", "2021 Il Harper");
+        WriteProperty(xmlWriter, "NSHighResolutionCapable", true);
+
+        xmlWriter.WriteEndElement();
+        xmlWriter.WriteEndElement();
+    }
+
+    static void WriteProperty(XmlWriter xmlWriter, string name, string value)
+    {
+        if (string.IsNullOrWhiteSpace(value)) return;
+        xmlWriter.WriteStartElement("key");
+        xmlWriter.WriteString(name);
+        xmlWriter.WriteEndElement();
+        xmlWriter.WriteStartElement("string");
+        xmlWriter.WriteString(value);
+        xmlWriter.WriteEndElement();
+    }
+
+    static void WriteProperty(XmlWriter xmlWriter, string name, bool value)
+    {
+        xmlWriter.WriteStartElement("key");
+        xmlWriter.WriteString(name);
+        xmlWriter.WriteEndElement();
+        xmlWriter.WriteStartElement(value ? "true" : "false");
+        xmlWriter.WriteEndElement();
+    }
+
+    #endregion
+}
