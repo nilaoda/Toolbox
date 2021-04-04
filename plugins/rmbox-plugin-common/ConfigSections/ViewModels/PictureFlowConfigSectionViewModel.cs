3e358f1ed9511b944350e9fee23f89cae895f157
Il Harper <afanyiyu@hotmail.com>
2021-04-04 15:24:46 +0800

    feat: Add PictureFlowConfigSection

---
+++ plugins/rmbox-plugin-common/ConfigSections/ViewModels/PictureFlowConfigSectionViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System.Linq;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.VisualTree;
+using Newtonsoft.Json;
+using ReactiveUI;
+using Ruminoid.Toolbox.Plugins.Common.ConfigSections.Views;
+using Ruminoid.Toolbox.Utils.Extensions;
+
+namespace Ruminoid.Toolbox.Plugins.Common.ConfigSections.ViewModels
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class PictureFlowConfigSectionViewModel : ReactiveObject
+    {
+        #region Constructor
+
+        public PictureFlowConfigSectionViewModel(
+            PictureFlowConfigSection view)
+        {
+            _view = view;
+
+            _hasInvalidCharHelper = this
+                .WhenAnyValue(x => x.Picture)
+                .Select(x => PathExtension.InvalidChars.Any(x.Contains))
+                .ToProperty(this, x => x.HasInvalidChar);
+        }
+
+        #endregion
+
+        #region View
+
+        private readonly PictureFlowConfigSection _view;
+
+        #endregion
+
+        #region Data
+
+        [JsonProperty("picture")]
+        private string _picture = "";
+
+        public string Picture
+        {
+            get => _picture;
+            set => this.RaiseAndSetIfChanged(ref _picture, value);
+        }
+
+        [JsonProperty("frame_rate")]
+        private int _frameRate = 30;
+
+        public int FrameRate
+        {
+            get => _frameRate;
+            set => this.RaiseAndSetIfChanged(ref _frameRate, value);
+        }
+
+        [JsonProperty("crf_value")]
+        private double _crfValue = 25;
+
+        public double CrfValue
+        {
+            get => _crfValue;
+            set => this.RaiseAndSetIfChanged(ref _crfValue, value);
+        }
+
+        [JsonProperty("size")]
+        private string _size = "1920x1080";
+
+        public string Size
+        {
+            get => _size;
+            set => this.RaiseAndSetIfChanged(ref _size, value);
+        }
+
+        [JsonProperty("duration")]
+        private int _duration = 60;
+
+        public int Duration
+        {
+            get => _duration;
+            set => this.RaiseAndSetIfChanged(ref _duration, value);
+        }
+
+        #endregion
+
+        #region Display
+
+        private readonly ObservableAsPropertyHelper<bool> _hasInvalidCharHelper;
+
+        public bool HasInvalidChar => _hasInvalidCharHelper.Value;
+
+        #endregion
+
+        #region Operations
+
+        public async Task PickFile()
+        {
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                AllowMultiple = false,
+                Title = "选择图片文件"
+            };
+            string[] result = await dialog.ShowAsync((Window)_view.GetVisualRoot());
+
+            if (!result.Any() ||
+                string.IsNullOrWhiteSpace(result[0]))
+                return;
+
+            Picture = result[0];
+        }
+
+        #endregion
+    }
+}
---
+++ plugins/rmbox-plugin-common/ConfigSections/Views/PictureFlowConfigSection.axaml.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Markup.Xaml;
+using Newtonsoft.Json.Linq;
+using Ruminoid.Toolbox.Core;
+using Ruminoid.Toolbox.Plugins.Common.ConfigSections.ViewModels;
+
+namespace Ruminoid.Toolbox.Plugins.Common.ConfigSections.Views
+{
+    [ConfigSection(
+        "Ruminoid.Toolbox.Plugins.Common.ConfigSections.PictureFlowConfigSection",
+        "一图流")]
+    public class PictureFlowConfigSection : ConfigSectionBase
+    {
+        public PictureFlowConfigSection()
+        {
+            throw new InvalidOperationException();
+        }
+
+        public PictureFlowConfigSection(
+            JToken sectionConfig)
+        {
+            DataContext = new PictureFlowConfigSectionViewModel(this);
+
+            InitializeComponent();
+
+            _pictureFileGrid = this.FindControl<Grid>("PictureFileGrid");
+
+            _pictureFileGrid.AddHandler(DragDrop.DropEvent, PictureDropHandler);
+        }
+
+        private void PictureDropHandler(object? sender, DragEventArgs e)
+        {
+            if (e.Data.Contains(DataFormats.Text))
+                (DataContext as PictureFlowConfigSectionViewModel).Picture = e.Data.GetText();
+            else if (e.Data.Contains(DataFormats.FileNames))
+                (DataContext as PictureFlowConfigSectionViewModel).Picture = (e.Data.GetFileNames() ?? Array.Empty<string>()).FirstOrDefault();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        private readonly Grid _pictureFileGrid;
+
+        public override object Config => DataContext as PictureFlowConfigSectionViewModel;
+    }
+}
