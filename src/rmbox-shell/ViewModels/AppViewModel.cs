0b9e100f0215408427576cf266879b3fa9b0761b
Il Harper <afanyiyu@hotmail.com>
2021-04-24 18:42:37 +0800

    feat: Add NativeMenu

---
+++ src/rmbox-shell/ViewModels/AppViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using ReactiveUI;
+using Ruminoid.Toolbox.Shell.Views;
+
+namespace Ruminoid.Toolbox.Shell.ViewModels
+{
+    public class AppViewModel : ReactiveObject
+    {
+        public void DoShowAboutWindow()
+        {
+            if (Application.Current.ApplicationLifetime is IClassicDesktopStyleApplicationLifetime
+            {
+                MainWindow: MainWindow mainWindow
+            })
+                AboutWindow.ShowAbout(mainWindow);
+        }
+    }
+}
