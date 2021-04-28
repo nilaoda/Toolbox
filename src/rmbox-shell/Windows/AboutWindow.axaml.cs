05ca2f80a2fad828f5738c86847e68eafdbbbb18
Il Harper <afanyiyu@hotmail.com>
2021-04-29 00:28:04 +0800

    feat: AboutWindow Update

--- src/rmbox-shell/Windows/AboutWindow.axaml.cs
+++ src/rmbox-shell/Windows/AboutWindow.axaml.cs
@@ -1,9 +1,30 @@
-﻿using System.Reflection;
+using System.Reflection;
+using Avalonia;
+using Avalonia.Markup.Xaml;
+using JetBrains.Annotations;
 using ReactiveUI;
-using Ruminoid.Toolbox.Shell.Views;
+using Ruminoid.Common2.Metro.MetroControls;
 
-namespace Ruminoid.Toolbox.Shell.ViewModels
+namespace Ruminoid.Toolbox.Shell.Windows
 {
+    public class AboutWindow : MetroWindow
+    {
+        public AboutWindow()
+        {
+            DataContext = new AboutWindowViewModel(this);
+
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+
     public class AboutWindowViewModel : ReactiveObject
     {
         public AboutWindowViewModel(
@@ -14,14 +35,17 @@ namespace Ruminoid.Toolbox.Shell.ViewModels
 
         private readonly AboutWindow _window;
 
+        [UsedImplicitly]
         public string VersionSummary { get; } = Assembly.GetExecutingAssembly().GetName().Version?.ToString();
 
+        [UsedImplicitly]
         public string VersionDetail { get; } = Assembly.GetExecutingAssembly()
             .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
             ?.InformationalVersion;
 
         #region Commands
 
+        [UsedImplicitly]
         public void DoCloseWindow()
         {
             _window.Close();
