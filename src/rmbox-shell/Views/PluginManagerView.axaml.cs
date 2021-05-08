fc5572d49b6371cd7b7b7252f9a70eb5a3ddbe2a
Il Harper <afanyiyu@hotmail.com>
2021-05-09 01:46:52 +0800

    refactor: PluginManagerView Update

--- src/rmbox-shell/Views/PluginManagerView.axaml.cs
+++ src/rmbox-shell/Views/PluginManagerView.axaml.cs
@@ -1,5 +1,12 @@
+using System.Collections.Generic;
+using System.Linq;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using JetBrains.Annotations;
+using ReactiveUI;
+using Ruminoid.Toolbox.Composition.Services;
+using Ruminoid.Toolbox.Core;
+using Splat;
 
 namespace Ruminoid.Toolbox.Shell.Views
 {
@@ -7,6 +14,8 @@ namespace Ruminoid.Toolbox.Shell.Views
     {
         public PluginManagerView()
         {
+            DataContext = new PluginManagerViewModel();
+
             InitializeComponent();
         }
 
@@ -14,5 +23,36 @@ namespace Ruminoid.Toolbox.Shell.Views
         {
             AvaloniaXamlLoader.Load(this);
         }
+    }
+
+    public class PluginManagerViewModel : ReactiveObject
+    {
+        #region Constructor
+
+        public PluginManagerViewModel()
+        {
+            IPluginService pluginService = Locator.Current.GetService<IPluginService>();
+            PluginList = pluginService.MetaCollection
+                .Select(x => x.Meta)
+                .ToList();
+        }
+
+        #endregion
+
+        #region Data
+
+        [UsedImplicitly]
+        public List<IMeta> PluginList { get; }
+
+        private IMeta _selectedPlugin;
+
+        [UsedImplicitly]
+        public IMeta SelectedPlugin
+        {
+            get => _selectedPlugin;
+            set => this.RaiseAndSetIfChanged(ref _selectedPlugin, value);
+        }
+
+        #endregion
     }
 }
