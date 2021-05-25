15426c36c3d838a12d4c832e1ba8a9a722a7d1c7
Il Harper <afanyiyu@hotmail.com>
2021-05-25 21:40:50 +0800

    feat: Add FFmpegAudioSilenceRemoveOperation

---
+++ plugins/rmbox-plugin-ffmpeg/Operations/FFmpegAudioSilenceRemoveOperation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Ruminoid.Common2.Utils.UserTypes;
+using Ruminoid.Toolbox.Core;
+using Ruminoid.Toolbox.Utils.Extensions;
+
+namespace Ruminoid.Toolbox.Plugins.FFmpeg.Operations
+{
+    [Operation(
+        "Ruminoid.Toolbox.Plugins.FFmpeg.Operations.FFmpegAudioSilenceRemoveOperation",
+        "去除静音",
+        "将音频中的无声部分裁剪去除。",
+        RateValue.ThreeStars,
+        "音频处理")]
+    public class FFmpegAudioSilenceRemoveOperation : IOperation
+    {
+        public List<TaskCommand> Generate(Dictionary<string, JToken> sectionData)
+        {
+            JToken ioSection =
+                sectionData[ConfigSectionBase.IOConfigSectionId];
+
+            string inputPath = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string outputPath = PathExtension.GetFullPathOrEmpty(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+
+            #region 自定义参数
+
+            JToken customArgsSection =
+                sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection"];
+
+            string customArgs = customArgsSection["args"]?.ToObject<string>();
+            bool useCustomArgs = customArgsSection["use_custom_args"]?.ToObject<bool>() ?? false;
+
+            #endregion
+
+            return new()
+            {
+                new(
+                    "ffmpeg",
+                    $"-i {inputPath} -y -af {(useCustomArgs ? customArgs : DefaultArgs)} {outputPath}",
+                    "ffmpeg")
+            };
+        }
+
+        private const string DefaultArgs = @"silenceremove=stop_periods=-1:stop_duration=1:stop_threshold=-40dB";
+
+        public Dictionary<string, JToken> RequiredConfigSections => new()
+        {
+            {
+                ConfigSectionBase.IOConfigSectionId,
+                JToken.FromObject(new
+                {
+                    output_suffix = "_processed"
+                })
+            },
+            {
+                "Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection",
+                JObject.FromObject(new
+                {
+                    default_args = DefaultArgs
+                })
+            }
+        };
+    }
+}
