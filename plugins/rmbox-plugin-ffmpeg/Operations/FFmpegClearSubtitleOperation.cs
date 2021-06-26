e753d1f22a9298198dfdefa7534b0235f4503d04
Il Harper <afanyiyu@hotmail.com>
2021-06-27 01:06:57 +0800

    feat: Add FFmpegClearSubtitleOperation

---
+++ plugins/rmbox-plugin-ffmpeg/Operations/FFmpegClearSubtitleOperation.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Ruminoid.Common2.Utils.UserTypes;
+using Ruminoid.Toolbox.Core;
+using Ruminoid.Toolbox.Utils.Extensions;
+
+namespace Ruminoid.Toolbox.Plugins.FFmpeg.Operations
+{
+    [Operation(
+        "Ruminoid.Toolbox.Plugins.FFmpeg.Operations.FFmpegClearSubtitleOperation",
+        "消除内封字幕",
+        "消除媒体文件中的内封字幕，但保留所有媒体轨道。",
+        RateValue.ThreeStars,
+        "封装转换")]
+    public class FFmpegClearSubtitleOperation : IOperation
+    {
+        public List<TaskCommand> Generate(Dictionary<string, JToken> sectionData)
+        {
+            JToken ioSection =
+                sectionData[ConfigSectionBase.IOConfigSectionId];
+
+            string inputPath = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string outputPath = PathExtension.GetFullPathOrEmpty(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+
+            #region 自定义参数
+
+            JToken customArgsSection =
+                sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection"];
+
+            string customArgs = customArgsSection["args"]?.ToObject<string>();
+            bool useCustomArgs = customArgsSection["use_custom_args"]?.ToObject<bool>() ?? false;
+
+            #endregion
+
+            return new()
+            {
+                new(
+                    "ffmpeg",
+                    $"-i {inputPath} -y {(useCustomArgs ? customArgs : DefaultArgs)} {outputPath}",
+                    "ffmpeg")
+            };
+        }
+
+        private const string DefaultArgs = "-map 0 -map -0:s -c copy";
+
+        public Dictionary<string, JToken> RequiredConfigSections => new()
+        {
+            {ConfigSectionBase.IOConfigSectionId, new JObject()},
+            {
+                "Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection",
+                JObject.FromObject(new
+                {
+                    default_args = DefaultArgs
+                })
+            }
+        };
+    }
+}
