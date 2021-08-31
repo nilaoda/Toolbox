ca22b99f83fe216aa03676b693e29133796f5308
Il Harper <afanyiyu@hotmail.com>
2021-09-01 00:24:43 +0800

    feat: add FFmpegVfrFixOperation

---
+++ plugins/rmbox-plugin-ffmpeg/Operations/FFmpegVfrFixOperation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Ruminoid.Common2.Utils.UserTypes;
+using Ruminoid.Toolbox.Core;
+using Ruminoid.Toolbox.Utils.Extensions;
+
+namespace Ruminoid.Toolbox.Plugins.FFmpeg.Operations
+{
+    [Operation(
+        "Ruminoid.Toolbox.Plugins.FFmpeg.Operations.FFmpegVfrFixOperation",
+        "修复音画不同步视频",
+        "修复音画不同步（VFR）视频。",
+        RateValue.ThreeStars,
+        "视频处理")]
+    public class FFmpegVfrFixOperation : IOperation
+    {
+        public List<TaskCommand> Generate(Dictionary<string, JToken> sectionData)
+        {
+            JToken ioSection =
+                sectionData[ConfigSectionBase.IOConfigSectionId];
+
+            string inputPath = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string outputPath = PathExtension.GetFullPathOrEmpty(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+
+            #region 自定义参数
+
+            JToken customArgsSection =
+                sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection"];
+
+            string customArgs = customArgsSection["args"]?.ToObject<string>();
+            bool useCustomArgs = customArgsSection["use_custom_args"]?.ToObject<bool>() ?? false;
+
+            #endregion
+
+            return new()
+            {
+                new(
+                    "ffmpeg",
+                    $"-y -i {inputPath} -c:v libx264 -vsync cfr {(useCustomArgs ? customArgs : DefaultArgs)} -af aresample=async=1000 -c:a aac {outputPath}",
+                    "ffmpeg")
+            };
+        }
+
+        private const string DefaultArgs = "-crf 21";
+
+        public Dictionary<string, JToken> RequiredConfigSections => new()
+        {
+            {
+                ConfigSectionBase.IOConfigSectionId,
+                JToken.FromObject(new
+                {
+                    output_suffix = "_fixed"
+                })
+            },
+            {
+                "Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection",
+                JObject.FromObject(new
+                {
+                    default_args = DefaultArgs
+                })
+            }
+        };
+    }
+}
