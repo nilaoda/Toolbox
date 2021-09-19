8eca2aa5c1ee51fd743eea25164f6604bc28e6b6
Il Harper <afanyiyu@hotmail.com>
2021-09-19 11:21:13 +0800

    feat: add BilibiliLoudnormOperation

---
+++ plugins/rmbox-plugin-ffmpeg/Operations/BilibiliLoudnormOperation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Ruminoid.Common2.Utils.UserTypes;
+using Ruminoid.Toolbox.Core;
+using Ruminoid.Toolbox.Utils.Extensions;
+
+namespace Ruminoid.Toolbox.Plugins.FFmpeg.Operations
+{
+    [Operation(
+        "Ruminoid.Toolbox.Plugins.FFmpeg.Operations.BilibiliLoudnormOperation",
+        "哔哩哔哩音频标准化",
+        "用于在哔哩哔哩投稿的音频标准化。视频也可以使用。",
+        RateValue.FourStars,
+        "音频处理")]
+    public class BilibiliLoudnormOperation : IOperation
+    {
+        public List<TaskCommand> Generate(Dictionary<string, JToken> sectionData)
+        {
+            JToken ioSection =
+                sectionData[ConfigSectionBase.IOConfigSectionId];
+
+            string inputPath = PathExtension.GetFullPathOrEmpty(ioSection["input"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+            string outputPath = PathExtension.GetFullPathOrEmpty(ioSection["output"]?.ToObject<string>() ?? string.Empty).EscapePathStringForArg();
+
+            #region 自定义参数
+
+            JToken customArgsSection =
+                sectionData["Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection"];
+
+            string customArgs = customArgsSection["args"]?.ToObject<string>();
+            bool useCustomArgs = customArgsSection["use_custom_args"]?.ToObject<bool>() ?? false;
+
+            #endregion
+
+            return new()
+            {
+                new(
+                    "ffmpeg",
+                    $"-i {inputPath} -y -c:v copy {(useCustomArgs ? customArgs : DefaultArgs)} {outputPath}",
+                    "ffmpeg")
+            };
+        }
+
+        private const string DefaultArgs = "-af loudnorm=i=-9:tp=-2 -b:a 320k";
+
+        public Dictionary<string, JToken> RequiredConfigSections => new()
+        {
+            {
+                ConfigSectionBase.IOConfigSectionId,
+                JToken.FromObject(new
+                {
+                    output_suffix = "_bilibili"
+                })
+            },
+            {
+                "Ruminoid.Toolbox.Plugins.Common.ConfigSections.CustomArgsConfigSection",
+                JObject.FromObject(new
+                {
+                    default_args = DefaultArgs
+                })
+            }
+        };
+    }
+}
